d537940979b1834c081f032f3c8cde95c2812c5c
Prajakta Darekar <114302180+prajudarekar04@users.noreply.github.com>
2023-09-08 01:01:10 +0530

    Completed Employee.java

--- AIP308 Java/Employee.java
+++ AIP308 Java/Employee.java
@@ -4,7 +4,6 @@ Write a program to develop class employee with constructor to initialize instanc
 Provide Set method and Get method for instance variables.
 Also provide a method to raise Salary of each employee by 10%.
 */
-//Not completed yet
 import java.util.*;	//for Scanner class
 public class Employee
 {
@@ -26,7 +25,7 @@ public class Employee
 	void setFirst()
 	{
 		Scanner sc = new Scanner(System.in);
-		System.out.println("First Name : ");
+		System.out.println("\nFirst Name : ");
 		firstName = sc.nextLine();
 	}
 	void setLast()
@@ -35,9 +34,27 @@ public class Employee
 		System.out.println("Last Name : ");
 		lastName = sc.nextLine();
 	}
-	void setSalaray()
+	void setSalary()
 	{
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Salary : ");
+		salary = sc.nextDouble();
+	}
+	void Display(double SalaryYear,double SalaryRised,double RisedSalaryNextYear)
+	{
+		System.out.println(firstName.concat(" ").concat(lastName) );
+		System.out.println("\nSalary Per Month:" + salary + "\nSalary per Year:" + SalaryYear + "\nRised Salary in Next Year:" + SalaryRised + "\nTotal Salary Rised next Year:" + RisedSalaryNextYear);
+	}
+	double CalculateSalary()
+	{
+		double SalaryYear = (salary * 12);
+		return SalaryYear;
+	}
+	Double RiseSalary()
+	{
+		double RisedSalary = ((salary *10)/100)+salary;
+		salary = RisedSalary;
+		return RisedSalary;
 	}
 	String getFirst()
 	{
@@ -47,13 +64,19 @@ public class Employee
 	{
 		return lastName;
 	}
-	double getSalaray()
+	double getSalary()
 	{
 		return salary;
 	}
 	public static void main(String[]args)
 	{
-		Employee e1 = new Employee();
+		Employee e1 = new Employee("Rajesh", "Wagle.", 45000);
+		System.out.println("\nFor First Employee : " + "\nName : " + e1.getFirst() + " " + e1.getLast());
+		System.out.println("Salary : " + e1.getSalary() + "\nAnnual Income : " + e1.CalculateSalary() +"\nRised Salary after a yr. : " + e1.RiseSalary());
+		
 		Employee e2 = new Employee();
+		e2.setFirst();	e2.setLast();	e2.setSalary();
+		System.out.println("\nFor Second Employee : " + "\nName : " + e2.getFirst() + " " + e2.getLast());
+		System.out.println("Salary : " + e2.getSalary() + "\nAnnual Income : " + e2.CalculateSalary() +"\nRised Salary after a yr. : " + e2.RiseSalary());
 	}
 }
