fa99f0ca4bf611d218dd3904442e0abc31a176f9
Prajakta Darekar <114302180+prajudarekar04@users.noreply.github.com>
2023-11-14 00:35:02 +0530

    Add files via upload

--- AIP308 Java/Matrix.java
+++ AIP308 Java/Matrix.java
@@ -1,14 +1,12 @@
-//Program for Matrix Operations using Java Class
 import java.util.*;
-public class Matrix
-{
-	public static void main(String [] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		int [][] a = new int [3][3];
+
+public class Matrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [][] a = new int [3][3];
 		int [][] b = new int [3][3];
 		int [][]res = new int [3][3];
-		System.out.println("Enter 1st Matrix:");
+        System.out.println("Enter 1st Matrix:");
 		for(int i =0;i<3;i++)
 		{
 			for(int j=0;j<3;j++)
@@ -20,9 +18,9 @@ public class Matrix
 			for(int j=0;j<3;j++)
 				b[i][j] = sc.nextInt();
 		}
-		while(true) {
+        while(true) {
 			int cho=0;
-			System.out.println("\nOptions:\n1.Add Matrix\n2.Subtract Matrix\n3.Multiply Matrix\n4.Exit\nEnter Choice:");
+			System.out.println("1.Add Matrix\n2.Subtract Matrix\n3.Multiply Matrix\n4.Exit\nEnter Choice:");
 			cho = sc.nextInt();
 			if(cho>=4)
 				break;
@@ -33,57 +31,47 @@ public class Matrix
 			if(cho==3)
 				MulMatrix(a,b,res);
 		}
-	}
-	static void AddMatrix(int [][] c,int [][] d,int[][] r)
-	{
-		for(int i =0;i<3;i++)
+    }
+    static void AddMatrix(int [][] c,int [][] d,int[][] r) {
+        for(int i = 0; i < 3; i++)
 		{
 			for(int j=0;j<3;j++)
 				r[i][j] = c[i][j] + d[i][j];
 		}
-		System.out.println("\nAddition of Matrices:");
-		for(int i =0;i<3;i++) {
-			System.out.println("");
-			for(int j=0;j<3;j++)
-				System.out.print(" "+r[i][j]);
+		for(int i = 0; i < 3; i++) {
+			System.out.print("\n");
+			for(int j = 0; j < 3; j++)
+				System.out.print(" " + r[i][j]);
 		}
 		System.out.println();
-	}
-	static void SubMatrix(int [][] c,int [][] d,int[][] r)
-	{
-		for(int i =0;i<3;i++)
+    }
+    static void SubMatrix(int [][] c,int [][] d,int[][] r) {
+        for(int i = 0; i < 3; i++)
 		{
 			for(int j=0;j<3;j++)
 				r[i][j] = c[i][j] - d[i][j];
 		}
-		System.out.println("\nSubtration of Matrices:");
-		for(int i =0;i<3;i++)
-		{
-			System.out.println("");
-			for(int j=0;j<3;j++)
-				System.out.print(" "+r[i][j]);
+		for(int i = 0; i < 3; i++) {
+			System.out.print("\n");
+			for(int j = 0; j < 3; j++)
+				System.out.print(" " + r[i][j]);
 		}
 		System.out.println();
-	}
-	static void MulMatrix(int [][] c,int [][] d,int[][] r)
-	{
-		for(int i =0;i<3;i++)
-		{
-			for(int j=0;j<3;j++)
-			{
-				for(int k=0;k<3;k++)      
-				{
-					r[i][j]+=c[i][k]*d[k][j];
-				}
-			}
-		}
-		System.out.println("\nMultiplication of Matrices:");
-		for(int i =0;i<3;i++)
-		{
-			System.out.println("");
-			for(int j=0;j<3;j++)
-				System.out.print(" "+r[i][j]);
-		}
-		System.out.println();
-	}
+    }
+    static void MulMatrix(int [][] c,int [][] d,int[][] r) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                r[i][j] = 0;
+                for (int k = 0; k < 3; k++) {
+                    r[i][j] += c[i][k] * d[k][j];
+                }
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            System.out.print("\n");
+            for (int j = 0; j < 3; j++)
+                System.out.print(" " + r[i][j]);
+        }
+        System.out.println();
+    }
 }
